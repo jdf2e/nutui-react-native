ac7951517874ff84b565166f5edc9a51ec547d68
HelloZhuziXiaoGe <472578439@qq.com>
2023-01-17 09:26:24 +0800

    feat:增加Layout组件

---
+++ components/col/col.tsx
@@ -0,0 +1,74 @@
+import React, {
+  FunctionComponent,
+  useEffect,
+  useState,
+  CSSProperties,
+} from 'react'
+import {
+  TouchableOpacity
+} from 'react-native';
+import  deviceWidth  from '../utils/deviceWidth'
+type EventType = 'row' | 'col'
+export interface ColProps {
+  span: string | number
+  offset: string | number
+  gutter: string | number
+  rowClassName: string
+  onPress: (e: any, type: EventType) => void
+}
+const defaultProps = {
+  span: '24',
+  offset: '0',
+  gutter: '0',
+  rowClassName: '',
+} as ColProps
+
+export const Col: FunctionComponent<
+  Partial<ColProps> & Omit<React.HTMLAttributes<HTMLDivElement>, 'onPress'>
+> = (props) => {
+  const { span, offset, children, rowClassName,gutter, onPress } = {
+    ...defaultProps,
+    ...props,
+  }
+  const [colName, setColName] = useState('')
+  const [colStyle, setColStyle] = useState({})
+
+  const classs = () => {
+    // 定义col的class类
+    const prefixCls = 'nut-col'
+    return `${prefixCls} ${prefixCls}-${span} ${
+      gutter ? `${prefixCls}-gutter` : ''
+    } ${prefixCls}-${offset}`
+  }
+  const getStyle = () => {
+    // 定义col的style类
+    
+    const style: CSSProperties = {}
+    style.paddingLeft = (gutter as number) / 2
+    style.paddingRight = (gutter as number) / 2,
+    style.width = (deviceWidth - (24/parseInt(span) -1) 
+    * parseInt(gutter))  * (parseInt(span)/24 ); 
+    if(offset){
+      style.marginTop = parseInt(offset);
+    }
+    return style
+  }
+  useEffect(() => {
+    setColName(classs)
+    setColStyle(getStyle)
+  }, [span, offset, gutter])
+
+  return (
+    <TouchableOpacity
+      style={{ ...colStyle }}
+      onPress={(e) => {
+        onPress && onPress(e, 'col')
+      }}
+    >
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+Col.defaultProps = defaultProps
+Col.displayName = 'NutCol'
---
+++ components/row/row.tsx
@@ -0,0 +1,162 @@
+import React, { FunctionComponent } from 'react'
+import {
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import  deviceWidth  from '../utils/deviceWidth'
+
+type EventType = 'row' | 'col'
+export interface RowProps {
+  type: string
+  justify: string
+  align: string
+  wrap: string
+  gutter: string | number
+  onPress: (e: any, type: EventType) => void
+}
+const defaultProps = {
+  type: '',
+  justify: 'start',
+  align: 'flex-start',
+  wrap: 'nowrap',
+  gutter: '0',
+  onPress:()=>{ }
+} as RowProps
+export const Row: FunctionComponent<
+  Partial<RowProps> & Omit<React.HTMLAttributes<HTMLDivElement>, 'onPress'>
+> = (props) => {
+  const { children, type, justify, align, wrap, gutter, onPress } = {
+    ...defaultProps,
+    ...props,
+  }
+
+  const getContainerStyle = () => {
+    let styles: any = {}
+    switch (justify) {
+      case 'start':
+        styles = {
+          justifyContent:'flex-start',
+        }
+        break
+      case 'end':
+        styles = {
+          justifyContent:'flex-end',
+        }
+        break
+      case 'center':
+      case 'space-around':
+      case 'space-between':
+      case 'space-evenly':
+        styles = {
+          justifyContent:justify,
+        }
+        break
+      default:
+        styles = {
+          justifyContent:justify,
+        }
+        break
+    }
+    return styles
+  }
+  
+  const getWrapStyle = () => {
+    let styles: any = {}
+    switch (wrap) {
+      case 'nowrap':
+      case 'wrap':
+        styles = {
+          flexWrap:wrap,
+        }
+        break
+      default:
+        styles = {
+          flexWrap:'nowrap',
+        }
+        break
+    }
+    return styles
+  }
+  
+
+  let cloneChildren: any = children
+  if (
+    Array.isArray(cloneChildren) ||
+    Object.prototype.toString.call(cloneChildren) === '[object Object]'
+  ) {
+    if (Array.isArray(cloneChildren)) {
+      if(gutter){
+        let returnView = [];
+        for (let i = 0;i<cloneChildren.length;i++){
+            if(i===cloneChildren.length-1){
+              returnView.push(React.cloneElement(cloneChildren[i], {
+                key: i,
+                gutter:gutter
+              }))
+            }else{
+              returnView.push(React.cloneElement(cloneChildren[i], {
+                key: i,
+                gutter:gutter
+              }))
+              returnView.push(
+                <View style={{width:parseInt(gutter)}}/>
+              )
+            }
+        }
+        if(wrap==='reverse'){
+          cloneChildren = returnView.reverse();
+        }else{
+          cloneChildren = returnView;
+        }
+      }else{
+        cloneChildren = cloneChildren.map((o: any, i: any) => {
+          return React.cloneElement(o, {
+            key: i,
+            gutter:gutter
+          })
+        })
+      }
+    } else {
+      cloneChildren = React.cloneElement(cloneChildren, {
+        gutter: gutter
+      })
+    }
+  }
+  if(type === 'flex'){
+    return (
+      <TouchableOpacity
+     style={[{
+        width:deviceWidth,
+        flexDirection:'row',
+        alignItems:align
+     },
+      getContainerStyle(),
+      getWrapStyle()
+    ]}
+    onPress={(e: any) => {
+      onPress && onPress(e, 'row')
+      }}
+    >
+      {cloneChildren}
+    </TouchableOpacity>)
+  }else{
+    return (
+      <TouchableOpacity
+        style={[{
+          width:deviceWidth,
+          flexDirection:'row'
+        },
+         getWrapStyle()
+        ]}
+      onPress={(e: any) => {
+        onPress && onPress(e, 'row')
+      }}
+    >
+      {cloneChildren}
+    </TouchableOpacity>)
+  }
+}
+
+Row.defaultProps = defaultProps
+Row.displayName = 'NutRow'
