554b6d41c138e1a6df4f0114b2f8c84fb57ee37e
Bin <124781335@qq.com>
2023-01-30 21:14:43 +0800

    fix: 添加组件导出

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,116 @@
+// 基础组件
 import Button from './button';
+import Cell from './cell';
+import CellGroup from './cellgroup';
 import ConfigProvider from './configprovider';
 import Icon from './icon';
+import Image from './image';
+import Overlay from './overlay';
+import Popup from './popup';
 import Portal from './portal';
-import { DemoPage, DemoCard } from './configprovider/styles/demo.page';
 
-export { Button, ConfigProvider, Icon, Portal, DemoPage, DemoCard };
+// 布局组件
+import Divider from './divider';
+import Grid from './grid';
+import GridItem from './griditem';
+import Row from './row';
+import Col from './col';
+
+// 导航组件
+import Elevator from './elevator';
+import FixedNav from './fixednav';
+import Navbar from './navbar';
+import Pagination from './pagination';
+import SideNavBar from './sidenavbar';
+import SubSideNavBar from './subsidenavbar';
+import SideNavBarItem from './sidenavbaritem';
+
+// 录入组件
+import Checkbox from './checkbox';
+import InputNumber from './inputnumber';
+import Radio from './radio';
+import Rate from './rate';
+import SearchBar from './searchbar';
+
+// 反馈组件
+import ActionSheet from './actionsheet';
+import BackTop from './backtop';
+import Dialog from './dialog';
+import Swipe from './swipe';
+import Switch from './switch';
+import Toast from './toast';
+
+// 展示组件
+import Avatar from './avatar';
+import Collapse from './collapse';
+import CollapseItem from './collapseitem';
+import CountDown from './countdown';
+import Empty from './empty';
+import NoticeBar from './noticebar';
+import Price from './price';
+import Skeleton from './skeleton';
+import Steps from './steps';
+import Step from './step';
+import Swiper from './swiper';
+import SwiperItem from './swiperitem';
+import Tag from './tag';
+import TrendArrow from './trendarrow';
+
+// 特色组件
+import TimeSelect from './timeselect';
+
+export {
+  Button,
+  ConfigProvider,
+  Icon,
+  Cell,
+  CellGroup,
+  Image,
+  Overlay,
+  Popup,
+  Portal,
+
+  Divider,
+  Grid,
+  GridItem,
+  Row,
+  Col,
+
+  Elevator,
+  FixedNav,
+  Navbar,
+  Pagination,
+  SideNavBar,
+  SubSideNavBar,
+  SideNavBarItem,
+
+  Checkbox,
+  InputNumber,
+  Radio,
+  Rate,
+  SearchBar,
+
+  ActionSheet,
+  BackTop,
+  Dialog,
+  Swipe,
+  Switch,
+  Toast,
+
+  Avatar,
+  Collapse,
+  CollapseItem,
+  CountDown,
+  Empty,
+  NoticeBar,
+  Price,
+  Skeleton,
+  Steps,
+  Step,
+  Swiper,
+  SwiperItem,
+  Tag,
+  TrendArrow,
+
+  TimeSelect,
+};
